c921d8a883f04612ee4846989c7444061512b0bd
Dcode <58519917+DCODE-ARMY@users.noreply.github.com>
2025-09-02 16:26:10 +0100

    Update mortgage_crew.py

--- mortgage_crew.py
+++ mortgage_crew.py
@@ -50,8 +50,6 @@ os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
 # import mlflow
 
 # mlflow.crewai.autolog()
-
-# # Optional: Set a tracking URI and an experiment name if you have a tracking server
 # mlflow.set_tracking_uri("http://localhost:5000")
 # mlflow.set_experiment("Mortgage_Crew_Experiment")
 
